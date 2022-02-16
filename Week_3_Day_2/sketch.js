2e36ef44ea55e9034ed093acc4ad1afe6a7c346a
Alanna Soepler <N00192732@iadt.ie>
2022-02-16 13:20:26 +0000

    Bar Chart Class

--- Week_3_Day_2/sketch.js
+++ Week_3_Day_2/sketch.js
@@ -13,12 +13,13 @@ let data = [
   { name: 'Apples', total: 43 },
 ];
 
-//this is an array function. It is NOT the same as the map function used further down
+//this is an array function/method. It is NOT the same as the map function used further down
 let listValues = data.map(function (x) {
   return x.total;
 });
 
-let scaledData = [];
+//Deceleration of variables
+//let scaledData = [];
 let numPlaces;
 let maxValue;
 let posX = 50;
@@ -30,101 +31,26 @@ let tickIncrements;
 let showValues = true;
 let showLabels = true;
 let rotateLabels = true;
+let showTicks = true;
 let colors;
 
 let tickSpacing = chartHeight / numTicks; //space between ticks on  the left
 let availableWidth = chartWidth - margin * 2 - spacing * (data.length - 1); //available space for bars
 let barWidth = availableWidth / data.length; //bar width
 
+let chart01;
+
 function setup() {
-  createCanvas(500, 500);
-  background(0);
-
-  colors = [
-    color('#ffe066'),
-    color('#fab666'),
-    color('#f68f6a'),
-    color('#f3646a'),
-  ];
-  maxValue = max(listValues);
-  tickIncrements = maxValue / numTicks;
-
-  for (let i = 0; i < data.length; i++) {
-    let tempVal = map(data[i].total, 0, maxValue, 0, chartHeight);
-    scaledData.push(tempVal);
-  }
+  createCanvas(800, 800);
+  chart01 = new BarChart(data);
+  chart01.chartWidth = 200;
+  chart01.chartHeight = 200;
+  chart01.posX = 100;
+  chart01.posY = 400;
+  chart01.updateValues();
 }
 
 function draw() {
-  background(0);
-
-  translate(posX, posY);
-  //chart
-  stroke(255, 180);
-  strokeWeight(1);
-  line(0, 0, 0, -chartHeight); //y
-  line(0, 0, chartWidth, 0); //x
-
-  for (let i = 0; i <= numTicks; i++) {
-    //ticks
-    stroke(255, 100);
-    line(0, tickSpacing * -i, -10, tickSpacing * -i);
-
-    //ticks
-    stroke(255, 100);
-    line(0, tickSpacing * -i, chartWidth, tickSpacing * -i);
-
-    //numbers (text)
-    fill(255, 200);
-    noStroke();
-    textSize(11);
-    textAlign(RIGHT, CENTER);
-    text((i * tickIncrements).toFixed(numPlaces), -15, tickSpacing * -i);
-  }
-
-  translate(margin, 0);
-  for (let i = 0; i < scaledData.length; i++) {
-    //Modules is the remainder.
-    //It will iterate though the colors and start from the beginning
-    let colorNumb = i % 4;
-
-    //bars
-    fill(colors[colorNumb]);
-    noStroke();
-    rect((barWidth + spacing) * i, 0, barWidth, -scaledData[i]);
-
-    //Bar Value
-    if (showValues) {
-      noStroke();
-      fill(255);
-      textSize(16);
-      textAlign(CENTER, BOTTOM);
-      text(
-        data[i].total,
-        (barWidth + spacing) * i + barWidth / 2,
-        -scaledData[i]
-      );
-    }
-
-    //Bar Label
-    if (showLabels) {
-      if (rotateLabels) {
-        push();
-        noStroke();
-        fill(255);
-        textSize(16);
-        textAlign(LEFT, CENTER);
-        translate((barWidth + spacing) * i + barWidth / 2, 10);
-        rotate(PI / 2);
-        text(data[i].name, 0, 0);
-        pop();
-      } else {
-        noStroke();
-        fill(255);
-        textSize(14);
-        textAlign(CENTER, BOTTOM);
-        text(data[i].name, (barWidth + spacing) * i + barWidth / 2, 20);
-      }
-    }
-  }
+  background(50);
+  chart01.render();
 }
