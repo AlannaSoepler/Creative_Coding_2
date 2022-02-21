1a98dedf5abe4578f3b7c12410b6e4f250655a65
Alanna Soepler <N00192732@iadt.ie>
2022-02-21 21:16:59 +0000

    Created Horizontal Bar Chart

--- Week_4_Horizontal_Bar_Chart/BarChart02.js
+++ Week_4_Horizontal_Bar_Chart/BarChart02.js
@@ -1,6 +1,140 @@
 class BarChart02 {
-    constructor(){
-        
+  constructor(_data) {
+    this.data = _data;
+    this.chartWidth = 400;
+    this.chartHeight = 400;
+    this.spacing = 10;
+    this.margin = 20;
+    this.numTicks = 10;
+    this.sizeTicks = 10;
+    this.textPlace = 25;
+    this.barValueSpacing = 10;
+    this.posX = 150;
+    this.posY = 450;
+    this.colors;
+    this.numPlaces = 0;
+    this.incrementTick;
+    this.maxVal;
 
+    this.showValues = true;
+    this.showLabels = true;
+    this.rotateLabels = false;
+
+    this.colors = [
+      color('#ffe066'),
+      color('#fab666'),
+      color('#f68f6a'),
+      color('#f3646a'),
+    ];
+    this.updateValues();
+    this.calculateMaxValue();
+  }
+  updateValues() {
+    this.tickSpacing = this.chartWidth / this.numTicks; //space between ticks on  the left
+    this.availableHight =
+      this.chartWidth - 2 * this.margin - (this.data.length - 1) * this.spacing; //available space for bars
+    this.barHight = this.availableHight / this.data.length; //bar width
+    this.completeSpacing = this.barHight + this.spacing;
+  }
+  calculateMaxValue() {
+    let listValues = this.data.map(function (x) {
+      return x.total;
+    });
+    this.maxVal = max(listValues);
+    this.incrementTick = Math.round(this.maxVal / this.numTicks);
+  }
+  render() {
+    push();
+    translate(this.posX, this.posY);
+    this.drawAxis();
+    this.drawTicks();
+    // this.drawHorizontalLine();
+    this.drawRects();
+    pop();
+  }
+  scaleData(num) {
+    return map(num, 0, this.maxVal, 0, this.chartHeight);
+  }
+  drawAxis() {
+    stroke(255, 0, 0, 200);
+    strokeWeight(2);
+    line(0, 0, 0, -this.chartHeight);
+    line(0, 0, this.chartWidth, 0);
+  }
+  drawTicks() {
+    for (let i = 0; i < this.numTicks; i++) {
+      let drawTick = this.chartWidth / this.numTicks;
+      //Draw the tick value
+      fill(255);
+      noStroke();
+      textSize(12);
+      textAlign(CENTER);
+      text(this.incrementTick * i, drawTick * i, this.textPlace);
+
+      stroke(255, 200);
+      line(this.tickSpacing * i, 0, this.tickSpacing * i, this.sizeTicks);
+
+      stroke(255, 50);
+      line(
+        this.tickSpacing * i,
+        -this.chartHeight,
+        this.tickSpacing * i,
+        this.sizeTicks
+      );
     }
+  }
+  drawRects() {
+    translate(0, -this.margin);
+    for (let i = 0; i < this.data.length; i++) {
+      let colorNumb = i % 4;
+
+      fill(this.colors[colorNumb]);
+      noStroke();
+      rect(
+        0,
+        -this.completeSpacing * i,
+        this.scaleData(this.data[i].total),
+        -this.barHight
+      );
+
+      if (this.showValues) {
+        noStroke();
+        fill(255);
+        textSize(16);
+        textAlign(LEFT, CENTER);
+        text(
+          this.data[i].total,
+          this.scaleData(this.data[i].total) + this.barValueSpacing,
+          -this.completeSpacing * i - this.barHight / 2
+        );
+      }
+
+      if (this.showLabels) {
+        if (this.otateLabels) {
+          push();
+          noStroke();
+          fill(255);
+          textSize(16);
+          textAlign(RIGHT, CENTER);
+          translate(
+            -this.barValueSpacing,
+            -this.completeSpacing * i - this.barHight / 2
+          );
+          rotate(PI / 2);
+          text(this.data[i].name, 0, 0);
+          pop();
+        } else {
+          noStroke();
+          fill(255);
+          textSize(16);
+          textAlign(RIGHT, BOTTOM);
+          text(
+            this.data[i].name,
+            -this.barValueSpacing,
+            -this.completeSpacing * i - this.barHight / 2
+          );
+        }
+      }
+    }
+  }
 }
--- Week_4_Horizontal_Bar_Chart/sketch.js
+++ Week_4_Horizontal_Bar_Chart/sketch.js
@@ -1,6 +1,3 @@
-let chartWidth = 400;
-let chartHeight = 400;
-
 //Array of objects
 let data = [
   { name: 'Oranges', total: 63 },
@@ -9,36 +6,8 @@ let data = [
   { name: 'Apples', total: 43 },
 ];
 
-let dataValues = [
-  { name: 'Oranges', total: 63 },
-  { name: 'Bananas', total: 33 },
-  { name: 'Pears', total: 7 },
-  { name: 'Apples', total: 43 },
-];
-
-let listValues = data.map(function (x) {
-  return x.total;
-});
-
-let numPlaces;
-let maxValue;
-let posX = 50;
-let posY = 450;
-let spacing = 10;
-let margin = 20;
-let numTicks = 10;
-let tickIncrements;
-let showValues = true;
-let showLabels = true;
-let rotateLabels = true;
-let showTicks = true;
-let colors;
-
-let tickSpacing = chartHeight / numTicks; //space between ticks on  the left
-let availableWidth = chartWidth - margin * 2 - spacing * (data.length - 1); //available space for bars
-let barWidth = availableWidth / data.length; //bar width
-
 let chart01;
+let chart02;
 
 function setup() {
   createCanvas(800, 800);
@@ -47,11 +16,19 @@ function setup() {
   chart01.chartWidth = 200;
   chart01.chartHeight = 200;
   chart01.posX = 50;
-  chart01.posY = 700;
+  chart01.posY = 500;
   chart01.updateValues();
+
+  chart02 = new BarChart02(data);
+  chart02.chartWidth = 200;
+  chart02.chartHeight = 200;
+  chart02.posX = 400;
+  chart02.posY = 500;
+  chart02.updateValues();
 }
 
 function draw() {
   background(50);
   chart01.render();
+  chart02.render();
 }
